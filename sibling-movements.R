29f7cc2155053a6a283b121f6fe02590ff7275fe
Jesse Sadler <jsadler@ucla.edu>
2018-04-16 13:08:46 -0700

    Simplify fill_dates function and clean script

--- sibling-movements.R
+++ sibling-movements.R
@@ -1,10 +1,9 @@
 library(tidyverse)
 library(timetk)
 library(xts)
-library(tibbletime)
-library(opencage)
+library(opencage) # development version
 library(lubridate)
-library(gganimate)
+
 
 sibling_movements <- read_csv("data-raw/sibling-movements.csv")
 siblings <- read_csv("data-raw/siblings.csv")
@@ -24,36 +23,44 @@ sibling_locations <- read_csv("data/sibling-locations.csv")
 vdms <- c("Andries van der Meulen", "Daniel van der Meulen", "Sara van der Meulen", "Anna van der Meulen")
 filter(sibling_movements, person %in% vdms)
 
-# Create numeric ids for place
+# Create numeric ids for place to keep this info when converted to xts
 sibling_movements_numeric <- sibling_movements %>% 
   left_join(sibling_locations, by = "place")
 
 # Function to fill dates
-fill_dates <- function(x) {
-  enquo(x)
-  person_xts <- sibling_movements_numeric %>% 
-    filter(person == !!x) %>% 
+# movement_df is movement data frame with columns that you want to keep as numbers
+# individual is the person to do this for, because dates have to be filled in for
+# one person at a time
+fill_dates <- function(movement_df, individual) {
+  # Convert to xts
+  person_xts <- movement_df %>% 
+    filter(person == individual) %>% 
     select(date, id) %>% 
     tk_xts(date_var = date)
   
+  # Fill in dates
   xts_fill <- na.locf(merge(person_xts, 
-                            seq(min(sibling_movements$date), 
-                                max(sibling_movements$date), by = 1)))
+                            seq(min(movement_df$date), 
+                                max(movement_df$date), by = 1)))
   
+  # Convert to tibble, filter one date per month, add back in information
   tk_tbl(xts_fill, rename_index = "date") %>% 
-    as_tbl_time(index = date) %>% 
-    as_period(period = "1 month") %>% 
-    as_tibble() %>% 
+    filter(mday(date) == 1) %>% 
     left_join(sibling_locations, by = "id") %>% 
-    mutate(person = !!x) %>% 
+    mutate(person = individual) %>% 
     select(person, place, date, lat, lng)
 }
 
-sibling_monthly <- map(vdms, fill_dates) %>% 
+# Create tibble of location of siblings per month and factor colum with date written out
+# Use map to do one person at a time
+sibling_monthly <- map(siblings$person, ~fill_dates(sibling_movements_numeric, .)) %>% 
   bind_rows() %>% 
   mutate(month = paste(month(date, label = TRUE), year(date))) %>% 
   mutate(month = as_factor(month))
 
+# How to create subset of sibling_monthly with correct number of factor levels
+filter(sibling_monthly, year(date) == 1585) %>% 
+  mutate(month = fct_drop(month))
 
 # Plots
 library(sf)
@@ -75,11 +82,10 @@ ggsave("sibling-movements.png", width = 6, height = 5)
 
 # Function for plot
 make_plot <- function(date_period) {
-  enquo(date_period)
   ggplot() + 
   geom_sf(data = countries_sf, fill = gray(0.75), color = gray(0.75)) + 
   coord_sf(xlim = c(-1, 11), ylim = c(50, 55), datum = NA) + 
-  geom_point(data = filter(sibling_monthly, month == !!date_period),
+  geom_point(data = filter(sibling_monthly, month == date_period),
              aes(x = lng, y = lat, color = person), 
              size = 3, position = position_dodge(width = 0.2)) +
   annotate("text", x = 10, y = 50, label = date_period, size = 5) + 
@@ -88,15 +94,20 @@ make_plot <- function(date_period) {
 }
 
 # For loop to make and save plots
+# Change save location by adding directory before "frame"
 p <- vector("list", length(levels(sibling_monthly$month)))
 
 for (i in 1:length(levels(sibling_monthly$month))) {
   p[[i]] <- make_plot(levels(sibling_monthly$month)[i])
-  png(paste0("vdm-movements/frame", i, ".png"), width = 1800, height = 1200, res = 300)
+  png(paste0("frame-", 
+             str_pad(i, width = 3, side = "left", pad = "0"),
+             ".png"),
+      width = 1800, height = 1200, res = 300)
   print(p[[i]])
   dev.off()
 }
 
+# Save a single png
 png("try1234.png", width = 1800, height = 1200, res = 300)
 print(p)
 dev.off()
